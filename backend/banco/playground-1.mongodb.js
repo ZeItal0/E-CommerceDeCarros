5b6d1455c6fd5e45a6acbc6bfb5f4924bb924d5c
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-05-16 20:24:32 -0300

    adição de playground para mongoDb for Vs Code

---
+++ backend/banco/playground-1.mongodb.js
@@ -0,0 +1,14 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("EcommerceVeiculos");
+
+// busca no banco sem filtros
+//db.Automoveis.find();
+
+// busca no banco com filtro
+//db.Automoveis.find({marca: "Toyota"});
+
+// busca no banco com filtro ignorando Letras Maiusculas e Minusculas
+//db.Automoveis.find({ marca: { $regex: "toyota", $options: "i" } });
