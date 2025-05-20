45ce7342e1aac2a931dbf581440b200731600232
Igor11405 <pedrribeirobr@gmail.com>
2025-05-20 16:18:26 -0300

    Feito o commit dos componentes (os codigos)

---
+++ frontend/src/components/UsadosItens.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import car from '../pages/cars/carros-esportivos.png'
+
+function UsadoItens() {
+  const [itens, setItens] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchItens = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/itens');
+        const data = await response.json();
+        setItens(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchItens();
+  }, []);
+
+  return (
+    <div className="itens">
+      {itens.length > 0 ? ( 
+        <ul>
+          {itens.filter((item) => item.status === 'usado').map((item) => (
+            <li key={item._id}>
+              <Link to={`/itens/${item._id}`} className='link-item'>
+                <img src={car} alt='cartest' className='carroimg' />
+                <a><strong>Marca:</strong> {item.marca} {item.modelo}</a>
+                <a><strong id='valorcolor'>Valor:</strong> {item.valor}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Automovel não encontrado</p>
+      )}
+    </div>
+  );
+}
+
+export default UsadoItens;
