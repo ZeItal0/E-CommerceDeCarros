5d1dd3279330c063dfb2a7e7560c401d99cb06df
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-05-20 22:40:40 -0300

    modificações dentro do banco de dados, tentativa de adição de imagens vinculadas aos veiculos no banco, alteração de nome das cores dos veiculos de português para ingles no banco de dados, adição de categoria dentro do banco de dados, criação de pasta ImagensVeiculos para acomodar as imagens dos veiculos, temporariamente, ainda estou estudando como coloca elas apenas dentro do banco sem a necessidade de uma pastar imagens

--- backend/banco/playground-1.mongodb.js
+++ backend/banco/playground-1.mongodb.js
@@ -12,3 +12,102 @@ use("EcommerceVeiculos");
 
 // busca no banco com filtro ignorando Letras Maiusculas e Minusculas
 //db.Automoveis.find({ marca: { $regex: "toyota", $options: "i" } });
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6820d9ed230d4148a5b50f7d") },
+//   {
+//     $set: {
+//       cor: "Silver",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/Hilux-Prata.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("682108913d9396f8773350a2") },
+//   {
+//     $set: {
+//       cor: "White",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/OnixLT-Branco",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("682108913d9396f8773350a3") },
+//   {
+//     $set: {
+//       cor: "Red",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/GolG6-Vermelho.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("682108913d9396f8773350a4") },
+//   {
+//     $set: {
+//       cor: "Gray",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/FiatStrada-Cinza.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6824fb3c5dd8b6192d947e17") },
+//   {
+//     $set: {
+//       cor: "Black",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/JeepCompass-Preto.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6824fc755dd8b6192d947e19") },
+//   {
+//     $set: {
+//       cor: "Gray",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/FiatUno-Cinza.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6824fd0c5dd8b6192d947e1b") },
+//   {
+//     $set: {
+//       cor: "Red",
+//       categoria: "Popular",
+//       imagem: "/ImagensVeiculos/PalioAttractive-Vermelho.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6824fde65dd8b6192d947e1c") },
+//   {
+//     $set: {
+//       cor: "Blue",
+//       categoria: "Esportivo",
+//       imagem: "/ImagensVeiculos/Bmw320I-Azul.png",
+//     },
+//   }
+// );
+
+// db.Automoveis.updateOne(
+//   { _id: ObjectId("6824fa4d5dd8b6192d947e15") },
+//   {
+//     $set: {
+//       cor: "Black",
+//       categoria: "Esportivo",
+//       imagem: "/ImagensVeiculos/Corolla-Preto.png",
+//     },
+//   }
+// );
